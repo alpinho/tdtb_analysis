13b49d8d7eed5972a4aa2e423df52ea351e5be08
Ana Luisa Pinho <anagpinho@gmail.com>
2022-05-22 16:19:33 -0400

    ENH: refactoring code

--- behavioral_data/data_parser.py
+++ behavioral_data/data_parser.py
@@ -16,22 +16,67 @@ import csv
 import numpy as np
 
 
-# subjects_no = [1, 2, 3, 4]
-subjects_no = [1]
+# %%
+# =========================== FUNCTIONS ================================
 
-subjects = ['sub-%02d' % s for s in subjects_no]
 
-# Load log file
+def parse_logfile(parent_dir, subject_no, task_name):
+    logpath = os.path.join(parent_dir, 'sub-%02d' % subject_no, 'logfiles')
+    logfiles = glob.glob(os.path.join(logpath, '*.xpd'))
+    logfiles.sort()
+    inputs_lists = [[line for line in csv.reader(open(logfile), delimiter=',')]
+                    for logfile in logfiles]
+    # Pick log file of selected task
+    for i, inputs_list in enumerate(inputs_lists, 1):
+        if inputs_list[6][0][9:] == task_name:
+            liste = inputs_list
+            break
+        if i == len(inputs_lists):
+            raise NameError('Log file for selected task does not exist!')
+    # Extract trial information from log file
+    for r, row in enumerate(liste):
+        if row[0] == str(subject_no):
+            break
+        else:
+            continue
+    trials_info = liste[r:]
+
+    return trials_info
+
+
+# %%
+# =========================== INPUTS ===================================
+
+# subjects = [1, 2, 3, 4]
+subjects = [1]
+
+# tasks = ['Auditory Production',
+#          'Auditory Perception',
+#          'Auditory No-Temporal Feature Discrimination',
+#          'Visual Production',
+#          'Visual Perception',
+#          'Visual No-Temporal Feature Discrimination']
+
+tasks = ['Auditory Production']
+
+# %%
+# ========================= PARAMETERS =================================
+
 this_dir = os.path.dirname(os.path.abspath(__file__))
-subject_logpath = os.path.join(this_dir, 'sub-01', 'logfiles')
-subject_logfiles = glob.glob(os.path.join(subject_logpath, '*.xpd'))
-subject_logfile = subject_logfiles[0]
-inputs_list = [line for line in csv.reader(open(subject_logfile), delimiter=',')]
 
-# Start parsing from the correct row
-for r, row in enumerate(inputs_list):
-    if row[0] == str(subjects_no[0]):
-        break
-    else:
-        continue
-data = inputs_list[r:]
+# Subject, task, condition, reaction time, response
+
+# %%
+# ============================ RUN =====================================
+
+if __name__ == "__main__":
+    for subject in subjects:
+        for task in tasks:
+            data = parse_logfile(this_dir, subject, task)
+            # ****************** Production Tasks **********************
+            # #### Signed Synchrony ####
+
+
+
+
+
