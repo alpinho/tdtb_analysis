f5463d1996318c539a9d15f6d6c9a1863e13f718
Ana Luisa Pinho <anagpinho@gmail.com>
2023-06-22 22:20:11 -0400

    FIXUP: psfiles + sub-14 ses-02/03 anomaly for fieldmap corr

--- imaging_analysis/psfiles.m
+++ imaging_analysis/psfiles.m
@@ -7,6 +7,6 @@ end
 % Rename and move postscript file
 old_psfile = dir('*.ps').name;
 old_psname = old_psfile(1:end-3);
-psfile = strcat(old_psname, '_' suffix '.ps');
+psfile = strcat(old_psname, ['_' suffix '.ps']);
 movefile(old_psfile, destination);
 movefile(fullfile(destination, old_psfile), fullfile(destination, psfile));
