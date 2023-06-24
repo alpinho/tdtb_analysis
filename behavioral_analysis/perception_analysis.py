99a6e4f5aa74528d2a926b306164f768bcbcbedb
Ana Luisa Pinho <anagpinho@gmail.com>
2023-06-23 22:22:53 -0400

    MISC: plotting updates + more data

--- behavioral_analysis/perception_analysis.py
+++ behavioral_analysis/perception_analysis.py
@@ -258,11 +258,11 @@ def individual_perception(
             colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red',
                       'tab:purple']
             if s == 0 and t == 0:
-                fig = plt.figure(figsize=(16, 140))
+                fig = plt.figure(figsize=(16, 200))
 
             # Define subplot of bar charts and its position in the fig
             # plt.axes([left, bottom, width, height])
-            ax = plt.axes([.1 + t*.46, .9685 - s*.025, .428, .0155])
+            ax = plt.axes([.1 + t*.46, .9685 - s*.02, .428, .0125])
 
             std_pse_audio = []
             std_dl_audio = []
@@ -367,7 +367,7 @@ def individual_perception(
                 all_pse_visual.append(std_pse_visual)
                 all_dl_visual.append(std_dl_visual)
 
-        fig.text(.03, .9765 - s*.025, 'Subject %d' % subject, ha='center',
+        fig.text(.03, .9765 - s*.02, 'Subject %d' % subject, ha='center',
                  fontsize=10, weight='bold')
 
     # Title
@@ -959,7 +959,7 @@ def threeway_repanova(df, this_dir, output_dir):
 
 SUBJECTS = [3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
             22, 23, 24, 25, 26, 27, 28, 29, 30, 32, 33, 34, 35, 36, 37, 38, 39,
-            40, 41, 42, 43]
+            40, 41, 42, 43, 44, 45]
 # SUBJECTS = [3, 7, 8, 10]
 
 # TASKS = ['Auditory Perception', 'Visual Perception']
