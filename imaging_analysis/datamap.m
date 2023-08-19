9a2767f3ed140a2d9016a5f84917bf5bcae705d1
Ana Luisa Pinho <anagpinho@gmail.com>
2023-08-19 06:08:24 -0400

    ENH: add sub-28 img ses-02 exceptions

--- imaging_analysis/datamap.m
+++ imaging_analysis/datamap.m
@@ -144,6 +144,32 @@ switch true
             ];
         raw_runs = {raw_runs_ses1, runs_ses2};
         preproc_runs = {preproc_runs_ses1, runs_ses2};
+        
+        
+    case strcmp(subject, 'sub-28')
+        
+        % Sessions
+        raw_ses = {"ses-01", "ses-02"};
+        preproc_ses = raw_ses;
+        
+        % Runs
+        runs_ses1 = [
+            "task-prod_run-01",   "task-prod_run-02",   ...
+            "task-percep_run-01", "task-percep_run-02", ...
+            "task-ntfd_run-01",   "task-ntfd_run-02"
+            ];
+        raw_runs_ses2 = [
+            "task-prod_run-01",   "task-prod_run-02",   ...
+            "task-percep_run-02", "task-percep_run-03", ...
+            "task-ntfd_run-01",   "task-ntfd_run-02", "task-ntfd_run-03", "task-ntfd_run-04"
+            ];
+        preproc_runs_ses2 = [
+            "task-prod_run-01",   "task-prod_run-02",   ...
+            "task-percep_run-01", "task-percep_run-02", ...
+            "task-ntfd_run-01",   "task-ntfd_run-02", "task-ntfd_run-03", "task-ntfd_run-04"
+            ];
+        raw_runs = {runs_ses1, raw_runs_ses2};
+        preproc_runs = {runs_ses1, preproc_runs_ses2};
 
  
     case strcmp(subject, 'sub-32')
