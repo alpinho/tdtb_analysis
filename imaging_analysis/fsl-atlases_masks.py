bee04e74c2dfb14dc9b2d56dcbc1564439e2e516
Ana Luisa Pinho <anagpinho@gmail.com>
2025-06-18 16:36:45 -0400

    ENH: add more fsl atlas regions

--- imaging_analysis/fsl-atlases_masks.py
+++ imaging_analysis/fsl-atlases_masks.py
@@ -4,7 +4,7 @@ This script creates masks from Harvard-Oxford subcortical atlas.
 Author: Ana Luisa Pinho
 
 Created: October 2023
-Last update: January 2024
+Last update: June 2025
 
 Compatibility: Python 3.10.14
 
@@ -61,138 +61,164 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 atlases_dir = os.path.join(working_dir, 'atlases')
 fsl_dir = os.path.join(atlases_dir, 'fsl_atlases')
 
+dstr_dir = os.path.join(fsl_dir, 'dstr')
+cerebellum_dir = os.path.join(fsl_dir, 'cerebellum')
+auditory_cortex_dir = os.path.join(fsl_dir, 'heschl_gyrus')
+
 hos_putamen_lh_probmap = os.path.join(
-    fsl_dir, 'harvardoxford-subcortical_prob_left_putamen.nii.gz')
+    dstr_dir, 'harvardoxford-subcortical_prob_left_putamen.nii.gz')
 hos_putamen_rh_probmap = os.path.join(
-    fsl_dir, 'harvardoxford-subcortical_prob_right_putamen.nii.gz')
+    dstr_dir, 'harvardoxford-subcortical_prob_right_putamen.nii.gz')
 hos_putamen_lh_maskpath = os.path.join(
-    fsl_dir, 'hos_putamen_lh_mask.nii.gz')
+    dstr_dir, 'hos_putamen_lh_mask.nii.gz')
 hos_putamen_rh_maskpath = os.path.join(
-    fsl_dir, 'hos_putamen_rh_mask.nii.gz')
+    dstr_dir, 'hos_putamen_rh_mask.nii.gz')
 
 hos_caudate_lh_probmap = os.path.join(
-    fsl_dir, 'harvardoxford-subcortical_prob_left_caudate.nii.gz')
+    dstr_dir, 'harvardoxford-subcortical_prob_left_caudate.nii.gz')
 hos_caudate_rh_probmap = os.path.join(
-    fsl_dir, 'harvardoxford-subcortical_prob_right_caudate.nii.gz')
+    dstr_dir, 'harvardoxford-subcortical_prob_right_caudate.nii.gz')
 hos_caudate_lh_maskpath = os.path.join(
-    fsl_dir, 'hos_caudate_lh_mask.nii.gz')
+    dstr_dir, 'hos_caudate_lh_mask.nii.gz')
 hos_caudate_rh_maskpath = os.path.join(
-    fsl_dir, 'hos_caudate_rh_mask.nii.gz')
+    dstr_dir, 'hos_caudate_rh_mask.nii.gz')
 
 hos_dstriatum_lh_maskpath = os.path.join(
-    fsl_dir, 'hos_dstr_lh_mask.nii.gz')
+    dstr_dir, 'hos_dstr_lh_mask.nii.gz')
 hos_dstriatum_rh_maskpath = os.path.join(
-    fsl_dir, 'hos_dstr_rh_mask.nii.gz')
+    dstr_dir, 'hos_dstr_rh_mask.nii.gz')
 
 hos_dstriatum_bh_maskpath = os.path.join(
-    fsl_dir, 'hos_dstr_bh_mask.nii.gz')
+    dstr_dir, 'hos_dstr_bh_mask.nii.gz')
 
 mniflirt_cereb6_lh_probmap = os.path.join(
-    fsl_dir, 'cerebellum_mniflirt_prob_leftVI.nii.gz')
+    cerebellum_dir, 'cerebellum_mniflirt_prob_leftVI.nii.gz')
 mniflirt_cereb6_rh_probmap = os.path.join(
-    fsl_dir, 'cerebellum_mniflirt_prob_rightVI.nii.gz')
+    cerebellum_dir, 'cerebellum_mniflirt_prob_rightVI.nii.gz')
 mniflirt_cereb6_lh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_cereb6_lh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_cereb6_lh_mask.nii.gz')
 mniflirt_cereb6_rh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_cereb6_rh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_cereb6_rh_mask.nii.gz')
 
 mniflirt_crus1_lh_probmap = os.path.join(
-    fsl_dir, 'cerebellum_mniflirt_prob_leftcrusI.nii.gz')
+    cerebellum_dir, 'cerebellum_mniflirt_prob_leftcrusI.nii.gz')
 mniflirt_crus1_rh_probmap = os.path.join(
-    fsl_dir, 'cerebellum_mniflirt_prob_rightcrusI.nii.gz')
+    cerebellum_dir, 'cerebellum_mniflirt_prob_rightcrusI.nii.gz')
 mniflirt_crus1_lh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_crus1_lh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_crus1_lh_mask.nii.gz')
 mniflirt_crus1_rh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_crus1_rh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_crus1_rh_mask.nii.gz')
 
 mniflirt_cereb7b_lh_probmap = os.path.join(
-    fsl_dir, 'cerebellum_mniflirt_prob_leftVIIb.nii.gz')
+    cerebellum_dir, 'cerebellum_mniflirt_prob_leftVIIb.nii.gz')
 mniflirt_cereb7b_rh_probmap = os.path.join(
-    fsl_dir, 'cerebellum_mniflirt_prob_rightVIIb.nii.gz')
+    cerebellum_dir, 'cerebellum_mniflirt_prob_rightVIIb.nii.gz')
 mniflirt_cereb7b_lh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_cereb7b_lh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_cereb7b_lh_mask.nii.gz')
 mniflirt_cereb7b_rh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_cereb7b_rh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_cereb7b_rh_mask.nii.gz')
 
 mniflirt_cereb8a_lh_probmap = os.path.join(
-    fsl_dir, 'cerebellum_mniflirt_prob_leftVIIIa.nii.gz')
+    cerebellum_dir, 'cerebellum_mniflirt_prob_leftVIIIa.nii.gz')
 mniflirt_cereb8a_rh_probmap = os.path.join(
-    fsl_dir, 'cerebellum_mniflirt_prob_rightVIIIa.nii.gz')
+    cerebellum_dir, 'cerebellum_mniflirt_prob_rightVIIIa.nii.gz')
 mniflirt_cereb8a_lh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_cereb8a_lh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_cereb8a_lh_mask.nii.gz')
 mniflirt_cereb8a_rh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_cereb8a_rh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_cereb8a_rh_mask.nii.gz')
 
 mniflirt_cereb7b8a_lh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_cereb7b8a_lh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_cereb7b8a_lh_mask.nii.gz')
 mniflirt_cereb7b8a_rh_maskpath = os.path.join(
-    fsl_dir, 'mniflirt_cereb7b8a_rh_mask.nii.gz')
+    cerebellum_dir, 'mniflirt_cereb7b8a_rh_mask.nii.gz')
+
+heschl_probmap = os.path.join(
+    auditory_cortex_dir, 'harvardoxford-cortical_prob_HeschlGyrus.nii.gz')
+heschl_maskpath = os.path.join(
+    auditory_cortex_dir, 'hos_heschl_mask.nii.gz')
+
+occipital_probmap = os.path.join(
+    auditory_cortex_dir, 'harvardoxford-cortical_prob_HeschlGyrus.nii.gz')
+occipital_maskpath = os.path.join(
+    auditory_cortex_dir, 'hos_occipital_mask.nii.gz')
 
 # ############################## RUN ####################################
 
 if __name__ == '__main__':
 
-    # # ######################## PUTAMEN ################################
-    hos_putamen_lh_bin = binarize(hos_putamen_lh_probmap, 50.)
-    hos_putamen_rh_bin = binarize(hos_putamen_rh_probmap, 50.)
+    # # # ######################## PUTAMEN ################################
+    # hos_putamen_lh_bin = binarize(hos_putamen_lh_probmap, 50.)
+    # hos_putamen_rh_bin = binarize(hos_putamen_rh_probmap, 50.)
 
-    # Save maks
-    hos_putamen_lh_bin.to_filename(hos_putamen_lh_maskpath)
-    hos_putamen_rh_bin.to_filename(hos_putamen_rh_maskpath)
+    # # Save masks
+    # hos_putamen_lh_bin.to_filename(hos_putamen_lh_maskpath)
+    # hos_putamen_rh_bin.to_filename(hos_putamen_rh_maskpath)
 
-    # # ######################## CAUDATE ###############################
-    hos_caudate_lh_bin = binarize(hos_caudate_lh_probmap, 50.)
-    hos_caudate_rh_bin = binarize(hos_caudate_rh_probmap, 50.)
+    # # # ######################## CAUDATE ###############################
+    # hos_caudate_lh_bin = binarize(hos_caudate_lh_probmap, 50.)
+    # hos_caudate_rh_bin = binarize(hos_caudate_rh_probmap, 50.)
 
-    # Save maks
-    hos_caudate_lh_bin.to_filename(hos_caudate_lh_maskpath)
-    hos_caudate_rh_bin.to_filename(hos_caudate_rh_maskpath)
+    # # Save masks
+    # hos_caudate_lh_bin.to_filename(hos_caudate_lh_maskpath)
+    # hos_caudate_rh_bin.to_filename(hos_caudate_rh_maskpath)
 
-    # # ###################### DORSAL STRIATUM #########################
-    combine_masks(hos_putamen_lh_maskpath, hos_caudate_lh_maskpath,
-                  hos_dstriatum_lh_maskpath)
+    # # # ###################### DORSAL STRIATUM #########################
+    # combine_masks(hos_putamen_lh_maskpath, hos_caudate_lh_maskpath,
+    #               hos_dstriatum_lh_maskpath)
 
-    combine_masks(hos_putamen_rh_maskpath, hos_caudate_rh_maskpath,
-                  hos_dstriatum_rh_maskpath)
+    # combine_masks(hos_putamen_rh_maskpath, hos_caudate_rh_maskpath,
+    #               hos_dstriatum_rh_maskpath)
 
-    combine_masks(hos_dstriatum_lh_maskpath, hos_dstriatum_rh_maskpath,
-                  hos_dstriatum_bh_maskpath)
+    # combine_masks(hos_dstriatum_lh_maskpath, hos_dstriatum_rh_maskpath,
+    #               hos_dstriatum_bh_maskpath)
 
-    # # ##################### CEREBELLUM VI ############################
-    mniflirt_cereb6_lh_bin = binarize(mniflirt_cereb6_lh_probmap, 50.)
-    mniflirt_cereb6_rh_bin = binarize(mniflirt_cereb6_rh_probmap, 50.)
+    # # # ##################### CEREBELLUM VI ############################
+    # mniflirt_cereb6_lh_bin = binarize(mniflirt_cereb6_lh_probmap, 50.)
+    # mniflirt_cereb6_rh_bin = binarize(mniflirt_cereb6_rh_probmap, 50.)
 
-    # Save maks
-    mniflirt_cereb6_lh_bin.to_filename(mniflirt_cereb6_lh_maskpath)
-    mniflirt_cereb6_rh_bin.to_filename(mniflirt_cereb6_rh_maskpath)
+    # # Save masks
+    # mniflirt_cereb6_lh_bin.to_filename(mniflirt_cereb6_lh_maskpath)
+    # mniflirt_cereb6_rh_bin.to_filename(mniflirt_cereb6_rh_maskpath)
 
-    # # ##################### CEREBELLUM CRUS I ########################
-    mniflirt_crus1_lh_bin = binarize(mniflirt_crus1_lh_probmap, 50.)
-    mniflirt_crus1_rh_bin = binarize(mniflirt_crus1_rh_probmap, 50.)
+    # # # ##################### CEREBELLUM CRUS I ########################
+    # mniflirt_crus1_lh_bin = binarize(mniflirt_crus1_lh_probmap, 50.)
+    # mniflirt_crus1_rh_bin = binarize(mniflirt_crus1_rh_probmap, 50.)
 
-    # Save maks
-    mniflirt_crus1_lh_bin.to_filename(mniflirt_crus1_lh_maskpath)
-    mniflirt_crus1_rh_bin.to_filename(mniflirt_crus1_rh_maskpath)
+    # # Save masks
+    # mniflirt_crus1_lh_bin.to_filename(mniflirt_crus1_lh_maskpath)
+    # mniflirt_crus1_rh_bin.to_filename(mniflirt_crus1_rh_maskpath)
 
-    # ###################### CEREBELLUM VIIb ###########################
-    mniflirt_cereb7b_lh_bin = binarize(mniflirt_cereb7b_lh_probmap, 50.)
-    mniflirt_cereb7b_rh_bin = binarize(mniflirt_cereb7b_rh_probmap, 50.)
+    # # ###################### CEREBELLUM VIIb ###########################
+    # mniflirt_cereb7b_lh_bin = binarize(mniflirt_cereb7b_lh_probmap, 50.)
+    # mniflirt_cereb7b_rh_bin = binarize(mniflirt_cereb7b_rh_probmap, 50.)
 
-    # Save maks
-    mniflirt_cereb7b_lh_bin.to_filename(mniflirt_cereb7b_lh_maskpath)
-    mniflirt_cereb7b_rh_bin.to_filename(mniflirt_cereb7b_rh_maskpath)
+    # # Save masks
+    # mniflirt_cereb7b_lh_bin.to_filename(mniflirt_cereb7b_lh_maskpath)
+    # mniflirt_cereb7b_rh_bin.to_filename(mniflirt_cereb7b_rh_maskpath)
 
-    # ##################### CEREBELLUM VIIIa ###########################
-    mniflirt_cereb8a_lh_bin = binarize(mniflirt_cereb8a_lh_probmap, 50.)
-    mniflirt_cereb8a_rh_bin = binarize(mniflirt_cereb8a_rh_probmap, 50.)
+    # # ##################### CEREBELLUM VIIIa ###########################
+    # mniflirt_cereb8a_lh_bin = binarize(mniflirt_cereb8a_lh_probmap, 50.)
+    # mniflirt_cereb8a_rh_bin = binarize(mniflirt_cereb8a_rh_probmap, 50.)
 
-    # Save maks
-    mniflirt_cereb8a_lh_bin.to_filename(mniflirt_cereb8a_lh_maskpath)
-    mniflirt_cereb8a_rh_bin.to_filename(mniflirt_cereb8a_rh_maskpath)
+    # # Save masks
+    # mniflirt_cereb8a_lh_bin.to_filename(mniflirt_cereb8a_lh_maskpath)
+    # mniflirt_cereb8a_rh_bin.to_filename(mniflirt_cereb8a_rh_maskpath)
 
-    # ############## COMBINE CEREBELLUM VIIb W/ VIIIa ##################
-    combine_masks(mniflirt_cereb7b_lh_maskpath, mniflirt_cereb8a_lh_maskpath,
-                  mniflirt_cereb7b8a_lh_maskpath)
+    # # ############## COMBINE CEREBELLUM VIIb W/ VIIIa ##################
+    # combine_masks(mniflirt_cereb7b_lh_maskpath, mniflirt_cereb8a_lh_maskpath,
+    #               mniflirt_cereb7b8a_lh_maskpath)
 
-    combine_masks(mniflirt_cereb7b_rh_maskpath, mniflirt_cereb8a_rh_maskpath,
-                  mniflirt_cereb7b8a_rh_maskpath)
+    # combine_masks(mniflirt_cereb7b_rh_maskpath, mniflirt_cereb8a_rh_maskpath,
+    #               mniflirt_cereb7b8a_rh_maskpath)
+
+    # ####################### HESCHL'S GYRUS #############################
+    hos_heschl_bin = binarize(heschl_probmap, 30.)
+
+    # Save mask
+    hos_heschl_bin.to_filename(heschl_maskpath)
+
+    # ####################### OCCIPITAL LOBE #############################
+    hos_heschl_bin = binarize(occipital_probmap, 50.)
+
+    # Save mask
+    hos_heschl_bin.to_filename(occipital_maskpath)
