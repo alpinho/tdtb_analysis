a68c5379fd45614a38c1ba593d9cd6c269a0f673
Ana Luisa Pinho <anagpinho@gmail.com>
2025-08-21 12:37:35 -0400

    CLEANUP: blank spaces

--- imaging_analysis/roi_extraction_msdtb.py
+++ imaging_analysis/roi_extraction_msdtb.py
@@ -507,26 +507,26 @@ roi_dir = os.path.join(
 contrast_type = 'wbmasked' # 'sm8wbmasked'
 
 # All ROIs: 10 ROIs
-atlas_dirnames10 = [fsl_dir, 
-                    ntk_dir, ntk_dir, ntk_dir,
-                    hmat_dir, hmat_dir, hmat_dir, hmat_dir,
-                    fsl_dir, 
-                    fsl_dir]
-atlas_names10 = ['hos', 
-                 'ntk_symmni128', 'ntk_symmni128', 'ntk_symmni128',
-                 'hmat', 'hmat', 'hmat', 'hmat',
-                 'hos', 
-                 'hos']
-region_names10 = ['dorsal_striatum', 
-                  'cerebellum', 'cerebellum', 'cerebellum',
-                  'motor_area', 'motor_area', 'motor_area', 'motor_area', 
-                  'heschl_gyrus', 
-                  'occipital_lobe']
-roi_names10 = ['dstr', 
-               'cereb-s', 'cereb-i', 'cereb',
-               'pmd', 'pmv', 'sma', 'presma',
-               'heschl',
-               'occipital']
+atlas_dirnames = [fsl_dir, 
+                  ntk_dir, ntk_dir, ntk_dir,
+                  hmat_dir, hmat_dir, hmat_dir, hmat_dir,
+                  fsl_dir, 
+                  fsl_dir]
+atlas_names = ['hos', 
+               'ntk_symmni128', 'ntk_symmni128', 'ntk_symmni128',
+               'hmat', 'hmat', 'hmat', 'hmat',
+               'hos', 
+               'hos']
+region_names = ['dorsal_striatum', 
+                'cerebellum', 'cerebellum', 'cerebellum',
+                'motor_area', 'motor_area', 'motor_area', 'motor_area', 
+                'heschl_gyrus', 
+                'occipital_lobe']
+roi_names = ['dstr', 
+             'cereb-s', 'cereb-i', 'cereb',
+             'pmd', 'pmv', 'sma', 'presma',
+             'heschl',
+             'occipital']
 
 # tags = ['i', 'a', 'g']
 tags = ['i', 'i9a', 'i8a', 'i7a', 'i6a', 'a', 'a4g', 'a3g', 'a2g', 'a1g', 'g']
