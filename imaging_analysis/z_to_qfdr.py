61f8b486292f712f2ff62374034e11689d2e2eda
Ana Luisa Pinho <anagpinho@gmail.com>
2026-07-07 02:26:27 -0400

    MAINT: params

--- imaging_analysis/z_to_qfdr.py
+++ imaging_analysis/z_to_qfdr.py
@@ -8,7 +8,7 @@ Author: Ana Luisa Pinho
 Email: agrilopi@uwo.ca
 
 Creation: 20th of January 2026
-Last Update: June 2026
+Last Update: July 2026
 
 Compatibility: Python 3.10.16, nilearn 0.11.1
 """
@@ -100,10 +100,10 @@ inputs_dir = os.path.join(home_dir,
                           'mygit', 'music_sdtb', 'tdtb_analysis',
                           'imaging_analysis', 'results')
 
-# group_functional = os.path.join(inputs_dir, 'parametric_tests', 'volume',
-#                                 'allmain_tasks')
 group_functional = os.path.join(inputs_dir, 'parametric_tests', 'volume',
-                                'rand_ntfd')
+                                'allmain_tasks')
+# group_functional = os.path.join(inputs_dir, 'parametric_tests', 'volume',
+#                                 'rand_ntfd')
 
 group_anat = os.path.join(inputs_dir, 'spm_volume_files', 'group_anat')
 
@@ -111,12 +111,12 @@ group_anat = os.path.join(inputs_dir, 'spm_volume_files', 'group_anat')
 # Inputs
 # =======================
 
-# zmap_path = os.path.join(group_functional, 
-#                          '1_encoding', '1_encoding_zmap.nii.gz')
+zmap_path = os.path.join(group_functional, 
+                         '1_encoding', '1_encoding_zmap.nii.gz')
 
-zmap_path = os.path.join(
-    group_functional, 
-    '17_random_vs_non-random', '17_random_vs_non-random_zmap.nii.gz')
+# zmap_path = os.path.join(
+#     group_functional, 
+#     '17_random_vs_non-random', '17_random_vs_non-random_zmap.nii.gz')
 
 # zmap_path = os.path.join(
 #     group_functional, 
@@ -126,10 +126,10 @@ zmap_path = os.path.join(
 wb_gmask = os.path.join(group_anat, 'group_mask_noskull.nii')
 
 # Z value to convert
-z_star = 4.87
+z_star = 2.80
 
 # "one" = one-sided (greater-than), "two" = two-sided
-side = "two"
+side = "one"
 
 # Only used to report the corresponding FDR Z-threshold
 alpha = 0.05
